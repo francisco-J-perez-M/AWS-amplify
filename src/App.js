1cf1b3bf95db7e3689b663a143bf67f8ede78a36
francisco-J-perez-M <franperez159264@gmail.com>
2025-02-08 16:26:51 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="https://preview.redd.it/add-john-xina-and-the-wok-as-generals-to-the-xinet-v0-8naylxusv5d81.jpg?width=225&format=pjpg&auto=webp&s=eaf0ceee1e92841f985d93586c9d8fe4d28757a9" className="App-logo" alt="logo" />
         <p>
-          main.
+          dev.
         </p>
       </header>
     </div>
