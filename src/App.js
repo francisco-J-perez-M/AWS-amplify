23448399eb12c4aa860e0a98c2f4be0c4c4fdc12
francisco jesus perez medina <franperez159264@gmail.com>
2025-02-08 12:45:43 -0600

    foto de john china

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://static.wikia.nocookie.net/theloudhousefanon/images/9/96/John_Xina.webp/revision/latest?cb=20230820051413" className="App-logo" alt="logo" />
         <p>
-          que pasa pa.
+          tu no tiene este.
         </p>
       </header>
     </div>
