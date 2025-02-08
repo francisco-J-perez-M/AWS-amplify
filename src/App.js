62da062860f40c38769b57d6a54bc2c1d2fd3ee1
francisco-J-perez-M <franperez159264@gmail.com>
2025-02-08 16:12:11 -0600

    imagen corregida

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://static.wikia.nocookie.net/memes-pedia/images/c/c1/John_Xina.png/revision/latest/thumbnail/width/360/height/360?cb=20211125075052&path-prefix=es" className="App-logo" alt="logo" />
+        <img src="https://preview.redd.it/add-john-xina-and-the-wok-as-generals-to-the-xinet-v0-8naylxusv5d81.jpg?width=225&format=pjpg&auto=webp&s=eaf0ceee1e92841f985d93586c9d8fe4d28757a9" className="App-logo" alt="logo" />
         <p>
           main.
         </p>
