39bd5f90e6e786eb1646c13fe58c156d59742479
francisco jesus perez medina <franperez159264@gmail.com>
2025-02-08 12:37:29 -0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          que pasa pa.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          que pasa pa
-        </a>
       </header>
     </div>
   );
